407645d332a03d8fd55c439d25c9b02f1e18272f
yetra <brdujmovic@gmail.com>
2021-05-18 22:10:07 +0200

    Add Vocab class

--- lab03/data.py
+++ lab03/data.py
@@ -6,3 +6,19 @@ class NLPDataItem:
     """Class representing an item in a dataset of texts."""
     text: str
     label: str
+
+
+class Vocab:
+    """Class for transforming tokens into integers."""
+
+    def __init__(self, frequencies, max_size, min_freq):
+        """
+        Inits a Vocab instance.
+
+        :param frequencies: a dict of dataset token frequencies
+        :param max_size: the largest number of tokens that can be stored
+        :param min_freq: the minimal frequency needed for storing a token
+        """
+        self.frequencies = frequencies
+        self.max_size = max_size
+        self.min_freq = min_freq
