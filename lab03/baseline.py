64e2479cc72510f9140724139eae279110016d16
yetra <brdujmovic@gmail.com>
2021-05-19 22:25:42 +0200

    Add main function

--- lab03/baseline.py
+++ lab03/baseline.py
@@ -1,6 +1,10 @@
 import torch
 import torch.nn as nn
+import torch.utils.data
+
 import numpy as np
+
+from data import NLPDataset, pad_collate
 
 
 class Baseline(nn.Module):
@@ -63,3 +67,35 @@ def evaluate(dataloader, model, loss_fn):
     correct /= size
     print(f'Test Error: \n Accuracy: {(100*correct):>0.1f}%, '
           f'Avg loss: {test_loss:>8f} \n')
+
+
+def main(seed=7052020, epochs=10):
+    """Performs SST sentiment analysis using Baseline."""
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+
+    train_dataloader = torch.utils.data.DataLoader(
+        dataset=NLPDataset.from_csv('data/sst_train_raw.csv'),
+        batch_size=10, shuffle=True, collate_fn=pad_collate)
+    valid_dataloader = torch.utils.data.DataLoader(
+        dataset=NLPDataset.from_csv('data/sst_valid_raw.csv'),
+        batch_size=32, shuffle=True, collate_fn=pad_collate)
+    test_dataloader = torch.utils.data.DataLoader(
+        dataset=NLPDataset.from_csv('data/sst_test_raw.csv'),
+        batch_size=32, shuffle=True, collate_fn=pad_collate)
+
+    model = Baseline()
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
+
+    for epoch in range(epochs):
+        print(f'Epoch {epoch}\n-------------------------------')
+        train(train_dataloader, model, loss_fn, optimizer)
+        evaluate(valid_dataloader, model, loss_fn)
+
+    print(f'Test data performance\n-------------------------------')
+    evaluate(test_dataloader, model, loss_fn)
+
+
+if __name__ == '__main__':
+    main()
