961052b771ca152b66318101f03c7d058df34dc4
yetra <brdujmovic@gmail.com>
2021-05-19 21:58:56 +0200

    Add baseline model

---
+++ lab03/baseline.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+
+
+class Baseline(nn.Module):
+    """
+    Baseline model for Stanford Sentiment Treebank sentiment analysis.
+
+    avg_pool() -> fc(300, 150) -> ReLU() -> fc(150, 150) -> ReLU() -> fc(150,1)
+    """
+
+    def __init__(self):
+        """Inits the baseline model."""
+        super().__init__()
+
+        self.modules = nn.Sequential(
+            nn.Linear(300, 150),
+            nn.ReLU(),
+            nn.Linear(150, 150),
+            nn.ReLU(),
+            nn.Linear(150, 1)
+        )
